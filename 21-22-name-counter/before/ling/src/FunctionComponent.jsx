c727e661c200bc22a545784310d0f5ea18ebb042
ling zhangm <lingzhangmyself@gmail.com>
2023-08-19 14:30:21 +1200

    refactor code to inline arrow fnc,the change happens inside setAge

--- 21-22-name-counter/before/ling/src/FunctionComponent.jsx
+++ 21-22-name-counter/before/ling/src/FunctionComponent.jsx
@@ -1,24 +1,35 @@
-import { useState } from "react";
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
 
 export default function FunctionComponent() {
   const [name, setName] = useState("Ling");
   const [age, setAge] = useState(0);
 
-  function handleChange() {
-    setName("Bella");
+  /*   function handleChange(e) {
+    console.log("e", e);
+    setName(e.target.value);
+    // setName("Bella");
   }
   function handleClickMinus() {
     setAge((currentAge) => currentAge - 1);
   }
   function handleClickPlus() {
     setAge((currentAge) => currentAge + 1);
-  }
+  } */
 
   return (
     <>
-      <input type="text" value={name} onChange={handleChange} />
+      {/*     <input type="text" value={name} onChange={handleChange} />
       <button onClick={handleClickMinus}>-</button> {age}
-      <button onClick={handleClickPlus}>+</button>
+      <button onClick={handleClickPlus}>+</button> */}
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={() => setAge((currentAge) => currentAge - 1)}>-</button>{" "}
+      {age}
+      <button onClick={() => setAge((currentAge) => currentAge + 1)}>+</button>
       <p>
         My name is {name} and I am {age} years old
       </p>
